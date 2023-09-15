bd1c10d245f31c43864b73c81f758c20575c66c1
Tunahan Çakıl <tcakil@kocfinans.com.tr>
2023-09-16 02:31:32 +0300

    Users changeable TOOO-Item backgroud color

--- src/WebUI/ClientApp/src/app/web-api-client.ts
+++ src/WebUI/ClientApp/src/app/web-api-client.ts
@@ -138,7 +138,7 @@ export class TodoItemsClient implements ITodoItemsClient {
             let result200: any = null;
             let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
                 result200 = resultData200 !== undefined ? resultData200 : <any>null;
-    
+
             return _observableOf(result200);
             }));
         } else if (status !== 200 && status !== 204) {
@@ -417,7 +417,7 @@ export class TodoListsClient implements ITodoListsClient {
             let result200: any = null;
             let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
                 result200 = resultData200 !== undefined ? resultData200 : <any>null;
-    
+
             return _observableOf(result200);
             }));
         } else if (status !== 200 && status !== 204) {
---
+++ src/WebUI/bin/Debug/net6.0/a953ec6244314a2697acff4e99265863.sh
@@ -0,0 +1,30 @@
+function list_child_processes(){
+    local ppid=$1;
+    local current_children=$(pgrep -P $ppid);
+    local local_child;
+    if [ $? -eq 0 ];
+    then
+        for current_child in $current_children
+        do
+          local_child=$current_child;
+          list_child_processes $local_child;
+          echo $local_child;
+        done;
+    else
+      return 0;
+    fi;
+}
+
+ps 52349;
+while [ $? -eq 0 ];
+do
+  sleep 1;
+  ps 52349 > /dev/null;
+done;
+
+for child in $(list_child_processes 52368);
+do
+  echo killing $child;
+  kill -s KILL $child;
+done;
+rm /Users/tunahanc/Desktop/testTaskFlying/src/WebUI/bin/Debug/net6.0/a953ec6244314a2697acff4e99265863.sh;
